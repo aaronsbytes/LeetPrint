cc30572bc81be0b83e07477fadb90f4dc7c0db14
aaronsbytes ~# ❚ <118199015+aaronsbytes@users.noreply.github.com>
2025-02-25 10:25:44 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using LeetPrint;
+
+Printer printer = new Printer();
+
+Console.WriteLine("Regular console output is boring, right?");
+
+Thread.Sleep(7000);
+
+printer.Print(
+    "But what about this one? This looks way cooler.",
+    delay: 3,
+    rounds: 1,
+    newLine: true,
+    symbols: "▒"
+);
+
+
+Thread.Sleep(7000);
+
+printer.Print(
+    "Or maybe this? Inspired by 'The Blacklist'",
+    delay: 1,
+    rounds: 10,
+    newLine: true
+);
+
+
